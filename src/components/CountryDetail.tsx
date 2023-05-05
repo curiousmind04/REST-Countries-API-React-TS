4798ddac88d47dc3a9e49e634857e6a121711b47
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-05-04 20:17:25 -0400

    finished desktop styling

--- src/components/CountryDetail.tsx
+++ src/components/CountryDetail.tsx
@@ -42,119 +42,125 @@ const CountryDetail: React.FC<Props> = ({ country, countries }) => {
   //   console.log(languagesData);
 
   return (
-    <>
+    <div className={classes.container}>
       <div className={classes.flag}>
         <img src={country.flags.png} />
       </div>
-      <h2>{country.name.common}</h2>
+      <div className={classes.right}>
+        <h2>{country.name.common}</h2>
+        <div className={classes.middle}>
+          <ul className={classes.geography}>
+            <li>
+              <h3>Native Name: </h3>
+              {country.name.nativeName ? (
+                <p>{Object.values(country.name.nativeName)[0].common}</p>
+              ) : (
+                <p>N/A</p>
+              )}
+            </li>
+            <li>
+              <h3>Population: </h3>
+              <p>{country.population.toLocaleString()}</p>
+            </li>
+            <li>
+              <h3>Region: </h3>
+              <p>{country.region}</p>
+            </li>
+            <li>
+              <h3>Sub Region: </h3>
+              {country.subregion ? <p>{country.subregion}</p> : <p>N/A</p>}
+            </li>
+            <li>
+              <h3>Capital: </h3>
+              {country.capital ? (
+                <div>
+                  {country.capital.map((cap) => (
+                    <p key={cap}>
+                      {country.capital.indexOf(cap) ===
+                      country.capital.length - 1
+                        ? `${cap}`
+                        : `${cap},`}
+                    </p>
+                  ))}
+                </div>
+              ) : (
+                <p>N/A</p>
+              )}
+            </li>
+          </ul>
 
-      <ul className={classes.geography}>
-        <li>
-          <h3>Native Name: </h3>
-          {country.name.nativeName ? (
-            <p>{Object.values(country.name.nativeName)[0].common}</p>
-          ) : (
-            <p>N/A</p>
-          )}
-        </li>
-        <li>
-          <h3>Population: </h3>
-          <p>{country.population.toLocaleString()}</p>
-        </li>
-        <li>
-          <h3>Region: </h3>
-          <p>{country.region}</p>
-        </li>
-        <li>
-          <h3>Sub Region: </h3>
-          {country.subregion ? <p>{country.subregion}</p> : <p>N/A</p>}
-        </li>
-        <li>
-          <h3>Capital: </h3>
-          {country.capital ? (
-            <div>
-              {country.capital.map((cap) => (
-                <p key={cap}>
-                  {country.capital.indexOf(cap) === country.capital.length - 1
-                    ? `${cap}`
-                    : `${cap},`}
-                </p>
-              ))}
-            </div>
-          ) : (
-            <p>N/A</p>
-          )}
-        </li>
-      </ul>
+          <ul className={classes.info}>
+            <li>
+              <h3>Top Level Domain: </h3>
+              {country.tld ? (
+                <div>
+                  {country.tld.map((item) => (
+                    <p key={item}>
+                      {country.tld.indexOf(item) === country.tld.length - 1
+                        ? `${item}`
+                        : `${item},`}
+                    </p>
+                  ))}
+                </div>
+              ) : (
+                <p>N/A</p>
+              )}
+            </li>
+            <li>
+              <h3>Currencies: </h3>
+              {country.currencies ? (
+                <div>
+                  {currenciesData.map((currency) => (
+                    <p className={classes.capitalize} key={currency}>
+                      {currenciesData.indexOf(currency) ===
+                      currenciesData.length - 1
+                        ? `${currency}`
+                        : `${currency},`}
+                    </p>
+                  ))}
+                </div>
+              ) : (
+                <p>N/A</p>
+              )}
+            </li>
+            <li>
+              <h3>Languages: </h3>
+              {country.languages ? (
+                <div>
+                  {languagesData.map((language) => (
+                    <p key={language}>
+                      {languagesData.indexOf(language) ===
+                      languagesData.length - 1
+                        ? `${language}`
+                        : `${language},`}
+                    </p>
+                  ))}
+                </div>
+              ) : (
+                <p>N/A</p>
+              )}
+            </li>
+          </ul>
+        </div>
 
-      <ul className={classes.info}>
-        <li>
-          <h3>Top Level Domain: </h3>
-          {country.tld ? (
-            <div>
-              {country.tld.map((item) => (
-                <p key={item}>
-                  {country.tld.indexOf(item) === country.tld.length - 1
-                    ? `${item}`
-                    : `${item},`}
-                </p>
+        <div className={classes.borders}>
+          <span>Border Countries: </span>
+          {country.borders === undefined && <span>N/A</span>}
+          {country.borders && (
+            <div className={classes.countries}>
+              {borderCountries.map((borderCountry) => (
+                <Link
+                  to={`/${borderCountry.name.common}`}
+                  key={borderCountry.name.common}
+                >
+                  {borderCountry.name.common}
+                </Link>
               ))}
             </div>
-          ) : (
-            <p>N/A</p>
           )}
-        </li>
-        <li>
-          <h3>Currencies: </h3>
-          {country.currencies ? (
-            <div>
-              {currenciesData.map((currency) => (
-                <p className={classes.capitalize} key={currency}>
-                  {currenciesData.indexOf(currency) ===
-                  currenciesData.length - 1
-                    ? `${currency}`
-                    : `${currency},`}
-                </p>
-              ))}
-            </div>
-          ) : (
-            <p>N/A</p>
-          )}
-        </li>
-        <li>
-          <h3>Languages: </h3>
-          {country.languages ? (
-            <div>
-              {languagesData.map((language) => (
-                <p key={language}>
-                  {languagesData.indexOf(language) === languagesData.length - 1
-                    ? `${language}`
-                    : `${language},`}
-                </p>
-              ))}
-            </div>
-          ) : (
-            <p>N/A</p>
-          )}
-        </li>
-      </ul>
-      <div className={classes.borders}>
-        <span>Border Countries: </span>
-        {country.borders === undefined && <span>N/A</span>}
-        {country.borders && (
-          <div className={classes.countries}>
-            {borderCountries.map((borderCountry) => (
-              <Link
-                to={`/${borderCountry.name.common}`}
-                key={borderCountry.name.common}
-              >
-                {borderCountry.name.common}
-              </Link>
-            ))}
-          </div>
-        )}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,92 +74,94 @@ const HomePage: React.FC<Props> = ({ countries }) => {
 
   return (
     <div>
-      <div className={classes.contain}>
-        <input
-          className={classes.search}
-          placeholder="Search for a country..."
-          ref={inputRef}
-          onChange={searchHandler}
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ionicon"
-          viewBox="0 0 512 512"
-        >
-          <path
-            d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
-            fill="none"
-            stroke="currentColor"
-            strokeMiterlimit="10"
-            strokeWidth="32"
+      <div className={classes.actions}>
+        <div className={classes.contain}>
+          <input
+            className={classes.search}
+            placeholder="Search for a country..."
+            ref={inputRef}
+            onChange={searchHandler}
           />
-          <path
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            strokeWidth="32"
-            d="M338.29 338.29L448 448"
-          />
-        </svg>
-      </div>
-
-      {showOptions && (
-        <div className={classes.backdrop} onClick={openFilterHandler}></div>
-      )}
-      <div className={classes.filter}>
-        <div
-          className={showOptions ? classes.open : classes.closed}
-          onClick={openFilterHandler}
-        >
-          <span>{option ? option : "Filter by region"}</span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="ionicon"
+            viewBox="0 0 512 512"
+          >
             <path
-              fill="currentColor"
-              d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+              d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
+              fill="none"
+              stroke="currentColor"
+              strokeMiterlimit="10"
+              strokeWidth="32"
+            />
+            <path
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              strokeWidth="32"
+              d="M338.29 338.29L448 448"
             />
           </svg>
         </div>
+
         {showOptions && (
-          <div className={classes.options}>
-            <div
-              className={option === "Africa" ? classes.chosen : ""}
-              onClick={optionHandler.bind(null, "Africa")}
-            >
-              Africa
-            </div>
-            <div
-              className={option === "Americas" ? classes.chosen : ""}
-              onClick={optionHandler.bind(null, "Americas")}
-            >
-              Americas
-            </div>
-            <div
-              className={option === "Antarctic" ? classes.chosen : ""}
-              onClick={optionHandler.bind(null, "Antarctic")}
-            >
-              Antarctic
-            </div>
-            <div
-              className={option === "Asia" ? classes.chosen : ""}
-              onClick={optionHandler.bind(null, "Asia")}
-            >
-              Asia
-            </div>
-            <div
-              className={option === "Europe" ? classes.chosen : ""}
-              onClick={optionHandler.bind(null, "Europe")}
-            >
-              Europe
-            </div>
-            <div
-              className={option === "Oceania" ? classes.chosen : ""}
-              onClick={optionHandler.bind(null, "Oceania")}
-            >
-              Oceania
-            </div>
-          </div>
+          <div className={classes.backdrop} onClick={openFilterHandler}></div>
         )}
+        <div className={classes.filter}>
+          <div
+            className={showOptions ? classes.open : classes.closed}
+            onClick={openFilterHandler}
+          >
+            <span>{option ? option : "Filter by region"}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path
+                fill="currentColor"
+                d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+              />
+            </svg>
+          </div>
+          {showOptions && (
+            <div className={classes.options}>
+              <div
+                className={option === "Africa" ? classes.chosen : ""}
+                onClick={optionHandler.bind(null, "Africa")}
+              >
+                Africa
+              </div>
+              <div
+                className={option === "Americas" ? classes.chosen : ""}
+                onClick={optionHandler.bind(null, "Americas")}
+              >
+                Americas
+              </div>
+              <div
+                className={option === "Antarctic" ? classes.chosen : ""}
+                onClick={optionHandler.bind(null, "Antarctic")}
+              >
+                Antarctic
+              </div>
+              <div
+                className={option === "Asia" ? classes.chosen : ""}
+                onClick={optionHandler.bind(null, "Asia")}
+              >
+                Asia
+              </div>
+              <div
+                className={option === "Europe" ? classes.chosen : ""}
+                onClick={optionHandler.bind(null, "Europe")}
+              >
+                Europe
+              </div>
+              <div
+                className={option === "Oceania" ? classes.chosen : ""}
+                onClick={optionHandler.bind(null, "Oceania")}
+              >
+                Oceania
+              </div>
+            </div>
+          )}
+        </div>
       </div>
 
       {!filteredCountries && countries && countries.length !== 0 && (
